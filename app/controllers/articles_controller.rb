4127cd6a80cab73b40b4150867350c31abd65802
adityabangari <adityabangari@qainfotech.com>
2017-07-06 12:26:12 +0530

    commiting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,6 @@ class ArticlesController < ApplicationController
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-
-     
 		if(@article.save)
 			redirect_to @article
 		else
